6c9ebe019e9ac16e70f2e349b6c7807185e299f9
pesa0015 <pesa0015@gmail.com>
2018-08-18 22:43:00 +0200

    Fix indentation in renderField component

--- src/components/renderField/renderField.js
+++ src/components/renderField/renderField.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const renderField = ({ input, label, type, meta: { touched, error } }) => (
-<div className="field">
-                    <label className="label left">{label}</label>
-                    <div className="control">
-                        <input className="input" {...input} type={type}/>
-      {touched && error && <span>{error}</span>}
-                    </div>
-                </div>
+    <div className="field">
+        <label className="label left">{label}</label>
+        <div className="control">
+            <input className="input" {...input} type={type}/>
+            {touched && error && <span>{error}</span>}
+        </div>
+    </div>
 )
 
 export default renderField;
