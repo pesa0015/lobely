48caa200a488dafcde8210960965a30cd9aef21e
pesa0015 <pesa0015@gmail.com>
2018-08-24 21:59:56 +0200

    Remove unused login service

--- src/services/login.js
+++
@@ -1,6 +0,0 @@
-import { http } from './httpHelper'
-
-export default function login(username, password) {
-    let payload = 'email=' + username + '&password=' + password;
-    return http.post('/auth', payload);
-}
