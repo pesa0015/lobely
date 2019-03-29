d39552f8951ed05068be391a1cbdb064c51b4eca
pesa0015 <pesa0015@gmail.com>
2019-03-29 22:59:37 +0100

    Use react-bulma-components Navbar for responsive menu

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -7,11 +7,12 @@ import Logout from './Logout'
 import './Nav.css'
 import heart from './../../assets/img/heart-liked.png'
 import message from './../../assets/img/message-white.png'
+import Navbar from 'react-bulma-components/lib/components/navbar';
 
 export class Nav extends Component {
     constructor(props) {
         super(props);
-        this.state = {title: '', books: [], chosenBook: false, slug: ''};
+        this.state = {title: '', books: [], chosenBook: false, slug: '', open: false};
         this.handleSearch = this.handleSearch.bind(this);
         this.chooseBook = this.chooseBook.bind(this);
     }
@@ -39,29 +40,34 @@ export class Nav extends Component {
     return (
       <div>
         <header>
-            <nav>
-                <input id="search-book" className="input" type="text" placeholder="Sök bok" onChange={this.handleSearch}/>
-                <div id='search-bar' style={(this.state.books.length === 0) ? {display: 'none'} : null }>
-                    {this.state.books.map((book, index) => (
-                        <Link to={'/title/' + book.slug} key={index} onClick={this.chooseBook}>{book.title}</Link>
-                    ))}
-                </div>
-                <div className="navbar-menu">
-                    <Link to="/messages"><div className="navbar-item"><img src={message} alt="Message"/>
-                    {(notifications.count.messages > 0 ? (
-                        <span className="notification-count">{notifications.count.messages}</span>
-                        ) : null
-                    )}</div></Link>
-                    <Link to="/notifications"><div className="navbar-item"><img src={heart} alt="Heart"/>
-                    {(notifications.count.hearts > 0 ? (
-                        <span className="notification-count">{notifications.count.hearts}</span>
-                        ) : null
-                    )}</div></Link>
-                    <Link to="/home"><div className="navbar-item">Hem</div></Link>
-                    <Link to="/profile"><div className="navbar-item">{ firstname }</div></Link>
-                    <div className="navbar-item"><Logout/></div>
-                </div>
-            </nav>
+            <Navbar active={this.state.open}>
+                <Navbar.Brand>
+                    <Navbar.Item renderAs='span'><Link to="/messages" onClick={() => this.setState({open: !this.state.open})}><img src={message} alt="Message"/>
+                        {(notifications.count.messages > 0 ? (
+                            <span className="notification-count">{notifications.count.messages}</span>
+                            ) : null
+                        )}</Link>
+                    </Navbar.Item>
+                    <Navbar.Item renderAs='span'><Link to="/notifications" onClick={() => this.setState({open: !this.state.open})}><img src={heart} alt="Heart"/>
+                        {(notifications.count.hearts > 0 ? (
+                            <span className="notification-count">{notifications.count.hearts}</span>
+                            ) : null
+                        )}</Link>
+                    </Navbar.Item>
+                    <Navbar.Burger active={this.state.open.toString()} onClick={() => this.setState({open: !this.state.open})}/>
+                </Navbar.Brand>
+                <Navbar.Menu>
+                    <input id="search-book" className="input" type="text" placeholder="Sök bok" onChange={this.handleSearch}/>
+                    <div id='search-bar' style={(this.state.books.length === 0) ? {display: 'none'} : null }>
+                        {this.state.books.map((book, index) => (
+                            <Link to={'/title/' + book.slug} key={index} onClick={this.chooseBook}>{book.title}</Link>
+                        ))}
+                    </div>
+                    <Navbar.Item renderAs='span'><Link to="/home" onClick={() => this.setState({open: !this.state.open})}>Hem</Link></Navbar.Item>
+                    <Navbar.Item renderAs='span'><Link to="/profile" onClick={() => this.setState({open: !this.state.open})}>{ firstname }</Link></Navbar.Item>
+                    <Navbar.Item renderAs='span'><Logout/></Navbar.Item>
+                </Navbar.Menu>
+            </Navbar>
         </header>
       </div>
     );
